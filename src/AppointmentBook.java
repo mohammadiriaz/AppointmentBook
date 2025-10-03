5ed0998f9af765be924084832e56b913604934ae
MohammadRiaz <mohammadr95@nycstudents.net>
2025-10-03 12:35:05 -0400

    Almost finished

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -1,40 +1,37 @@
-public class AppointmentBook
-{
+public class AppointmentBook {
     private boolean[][] schedule;
-    public AppointmentBook(boolean[][] schedule)
-    {
+
+    public AppointmentBook(boolean[][] schedule) {
         this.schedule = schedule;
     }
-}
 
-    private boolean isMinuteFree(int period, int minute)
-    {
-        return schedule[period -1][minute];
+
+    private boolean isMinuteFree(int period, int minute) {
+        return schedule[period - 1][minute];
     }
 
-    private void reserveBlock(int period, int startMinute, int duration)
-    { /* implementation not shown */ }
+    private void reserveBlock(int period, int startMinute, int duration) { /* implementation not shown */ }
 
-    public int findFreeBlock(int period, int duration){
+    public int findFreeBlock(int period, int duration) {
         int block = 0;
-        for(int i = 0; i < 60; i++)
-            if(isMinuteFree(period, i))
-            {
+        for (int i = 0; i < 60; i++)
+            if (isMinuteFree(period, i)) {
+                block++;
+                if(block == duration);
+                return i - duration + 1;
 
             }
     }
 
-    public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration) {
         return false;
     }
-public void printPeriod(int period)
-{
-    for(int i = 0; i < schedule[period - 1].length; i++)
-        System.out.println(i + " " + schedule[period - 1][i]);
-}
-    { /* to be implemented in part (b) */ }
-// There may be instance variables, constructors, and methods that are not shown.
-}
 
-void main() {
+    public void printPeriod(int period) {
+        for (int i = 0; i < schedule[period - 1].length; i++)
+            System.out.println(i + " " + schedule[period - 1][i]);
+    }
+
+    void main() {
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-public class Main {
+
+public class Main
+{
     public static void main(String[] args){
 
 boolean[][] schedule = new boolean [8][60];
 AppointmentBook a = new AppointmentBook(schedule);
 for(int i = 10; i < 15; i++) schedule[1][i] = true;
-a.printPeriod(2);}}
+a.printPeriod(2);
+    }
+}
 
 
 
